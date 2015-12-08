bbda28307c3efbaf1c95a8090951ca184aedaa06
Tobias Vogel <tobias.vogel@idealo.de>
2015-12-08 13:40:32 +0100

    make TabularDataSource working

--- main.rb
+++ main.rb
@@ -1,6 +1,6 @@
-#!/bin/ruby
 require_relative "src/MergeContacts.class.rb"
 mc = MergeContacts.new ARGV
 mc.run
 
-# call main.rb abc.tsv tsv:blabla.tsv csv:blabla.csv vcard:contacts.vcard
+# call it like so (replace ruby with your preferred version that is sufficiently recent):
+# ruby main.rb abc.tsv tsv:blabla.tsv csv:blabla.csv vcard:contacts.vcard
