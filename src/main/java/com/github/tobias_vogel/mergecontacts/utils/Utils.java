0b12fa70342c46219201f9cf6a4d3cd8b6125ae0
Tobias Vogel <tobias@vogel.name>
2016-01-29 06:37:25 +0100

    add utils class

---
+++ src/main/java/com/github/tobias_vogel/mergecontacts/utils/Utils.java
@@ -0,0 +1,16 @@
+package com.github.tobias_vogel.mergecontacts.utils;
+
+public class Utils {
+
+    // def Utils.failsafeArrayHashAppend(hash, key, value)
+    // if hash.member?(key)
+    // entry = hash[key]
+    // entry << value
+    // hash[key] = entry
+    // else
+    // hash[key] = [value]
+    // end
+    // return hash
+    // end
+    // end
+}
