1ee1795e1e649afd125f9ae6bcdc27c6ab5c607f
Tobias Vogel <tobias@vogel.name>
2016-03-09 07:29:51 +0100

    parse command line parameters with jcommander (untested)

--- src/main/java/com/github/tobias_vogel/mergecontacts/MergeContacts.java
+++ src/main/java/com/github/tobias_vogel/mergecontacts/MergeContacts.java
@@ -1,80 +1,72 @@
 package com.github.tobias_vogel.mergecontacts;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Set;
 
+import com.beust.jcommander.JCommander;
 import com.github.tobias_vogel.mergecontacts.data.CardDavContact;
 import com.github.tobias_vogel.mergecontacts.datasources.FileLoaderAndClenser;
 import com.github.tobias_vogel.mergecontacts.merging.MergeCoordinator;
+import com.github.tobias_vogel.mergecontacts.utils.CommandlineParser;
 
 class MergeContacts {
 
-    private static String mainContactFileSpecifier;
+	private static String mainContactFileSpecifier;
 
-    private static Set<CardDavContact> mainContacts;
+	private static Set<CardDavContact> mainContacts;
 
-    private static String[] additionalContactsFileSpecifiers;
+	private static List<String> additionalContactsFileSpecifiers;
 
-    private static Set<CardDavContact> additionalContacts;
+	private static Set<CardDavContact> additionalContacts;
 
-    private static String targetFilename;
+	private static String targetFilename;
 
+	private static boolean overwriteTargetFile;
 
+	public static void main(String[] args) {
+		parseFileSpecifiersFromCommandLineArguments(args);
 
+		loadAndClenseContactsFromFiles();
 
+		mergeContacts();
 
-    public static void main(String[] args) {
-        parseFileSpecifiersFromCommandLineArguments(args);
+		// TODO respect an overwrite flag that allows to overwrite the possibly
+		// existing target file
+		exportContactsToTsv();
 
-        loadAndClenseContactsFromFiles();
+		System.out.println("Finished");
+	}
 
-        mergeContacts();
+	private static void parseFileSpecifiersFromCommandLineArguments(String[] commandlineArguments) {
+		CommandlineParser commandlineParser = new CommandlineParser();
+		new JCommander(commandlineParser, commandlineArguments);
 
-        exportContactsToTsv();
+		mainContactFileSpecifier = commandlineParser.getMainFileSpecifier();
+		additionalContactsFileSpecifiers = commandlineParser.getAdditionalFileSpecifiers();
+		targetFilename = commandlineParser.getTargetFilename();
+		overwriteTargetFile = commandlineParser.overwriteTargetFile();
 
-        System.out.println("Finished");
-    }
+		// fake result
+		// TODO remove fake results
+		mainContactFileSpecifier = "CSV:data/blabla.csv";
+		additionalContactsFileSpecifiers = Arrays.asList(new String[] { "TSV:data/huhu.tsv", "VCARD:data/haha.vcard" });
+	}
 
+	private static void loadAndClenseContactsFromFiles() {
+		mainContacts = FileLoaderAndClenser.loadFileAndClenseContacts(mainContactFileSpecifier);
 
+		for (String additionalFileSpecifier : additionalContactsFileSpecifiers) {
+			additionalContacts.addAll(FileLoaderAndClenser.loadFileAndClenseContacts(additionalFileSpecifier));
+		}
+	}
 
+	private static void mergeContacts() {
+		mainContacts = MergeCoordinator.coordinateMerge(mainContacts, additionalContacts);
+	}
 
-
-    private static void parseFileSpecifiersFromCommandLineArguments(String[] filenames) {
-        // TODO use jcommander to parse command line arguments
-
-        // TODO respect an overwrite flag that allows to overwrite the possibly
-        // existing target file
-
-        // fake result
-        mainContactFileSpecifier = "CSV:data/blabla.csv";
-        additionalContactsFileSpecifiers = new String[] { "TSV:data/huhu.tsv", "VCARD:data/haha.vcard" };
-    }
-
-
-
-
-
-    private static void loadAndClenseContactsFromFiles() {
-        mainContacts = FileLoaderAndClenser.loadFileAndClenseContacts(mainContactFileSpecifier);
-
-        for (String additionalFileSpecifier : additionalContactsFileSpecifiers) {
-            additionalContacts.addAll(FileLoaderAndClenser.loadFileAndClenseContacts(additionalFileSpecifier));
-        }
-    }
-
-
-
-
-
-    private static void mergeContacts() {
-        mainContacts = MergeCoordinator.coordinateMerge(mainContacts, additionalContacts);
-    }
-
-
-
-
-
-    private static void exportContactsToTsv() {
-        // TODO implement
-        System.out.println("would (perhaps) write to " + targetFilename);
-    }
+	private static void exportContactsToTsv() {
+		// TODO implement
+		System.out.println("would (perhaps) write to " + targetFilename);
+	}
 }
---
+++ src/main/java/com/github/tobias_vogel/mergecontacts/utils/CommandlineParser.java
@@ -0,0 +1,41 @@
+package com.github.tobias_vogel.mergecontacts.utils;
+
+import java.util.List;
+
+import com.beust.jcommander.Parameter;
+
+public class CommandlineParser {
+
+	@Parameter(names = { "--main, -m" }, description = "Main contact file specifier (wins on merge ties)")
+	private String mainFileSpecifier;
+	
+	@Parameter(names = { "--target, -t" }, description = "The target filename")
+	private String targetFilename;
+	
+	@Parameter(names = { "--overwrite, -o" }, description = "Whether or not to overwrite the target file if it exists")
+	private boolean overwriteTargetFile = false;
+	
+	@Parameter(names = { "--additional, -a" }, description = "Additional contact file specifier")
+	private List<String> additionalFileSpecifiers;
+
+	
+	
+	public String getMainFileSpecifier() {
+		return mainFileSpecifier;
+	}
+
+
+	public String getTargetFilename() {
+		return targetFilename;
+	}
+
+
+	public boolean overwriteTargetFile() {
+		return overwriteTargetFile;
+	}
+
+
+	public List<String> getAdditionalFileSpecifiers() {
+		return additionalFileSpecifiers;
+	}
+}
